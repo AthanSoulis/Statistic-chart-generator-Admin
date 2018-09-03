663c1f57d5ba6150cb950395be149a5e1708878a
Athan Soulis <athan.soulis@gmail.com>
2018-09-03 12:45:57 +0300

    Updated Entity Field Selection Widget
    Entity fields now subscribe to the entity field value changes in order to be able to update the available field selections

--- src/app/custom-ngx-schema-form-widgets/entity-field-selection-widget/entity-field-selection-widget.component.ts
+++ src/app/custom-ngx-schema-form-widgets/entity-field-selection-widget/entity-field-selection-widget.component.ts
@@ -1,24 +1,36 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterContentInit } from '@angular/core';
 import { ControlWidget } from 'ngx-schema-form';
 import { EntitySelectionWidgetService } from '../entity-selection-widget/entity-selection-widget.component';
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subscription, Subject } from 'rxjs';
+import { ObjectProperty } from 'ngx-schema-form/lib/model/objectproperty';
+import { PropertyGroup } from 'ngx-schema-form/lib/model/formproperty';
+import { FieldNode } from '../../services/db-schema-service/db-schema.service';
 
 @Component({
   selector: 'entity-field-selection-widget',
   templateUrl: './entity-field-selection-widget.component.html',
   styleUrls: ['./entity-field-selection-widget.component.css']
 })
-export class EntityFieldSelectionWidgetComponent extends ControlWidget implements OnDestroy {
+export class EntityFieldSelectionWidgetComponent extends ControlWidget implements OnDestroy, AfterContentInit {
 
   entityValue: string;
   private entitySubscription: Subscription;
 
-  constructor( private entitySelectionService: EntitySelectionWidgetService ) {
+  constructor() {
     super();
+  }
 
-    this.entitySubscription = this.entitySelectionService.entity.subscribe(
-      (data: string) => this.entityValue = data
-    );
+  ngAfterContentInit() {
+
+    let property = this.formProperty;
+    while (property.schema.title !== 'Data Selection') {
+      property = property.parent;
+    }
+    this.entitySubscription = (<PropertyGroup>(<ObjectProperty>(<ObjectProperty>property).getProperty('yaxisData')).getProperty('entity'))
+                              .valueChanges.asObservable().subscribe(
+                                (data: string) => this.entityValue = data
+                              );
+
   }
 
   ngOnDestroy() {
