9213713052b63933c4abdb3ee19b4cc1eb04346a
Athan Soulis <athan.soulis@gmail.com>
2018-09-03 12:58:18 +0300

    Updated the Operator Selection Implementation

--- src/app/custom-ngx-schema-form-widgets/operator-selection-widget/operator-selection-widget.component.ts
+++ src/app/custom-ngx-schema-form-widgets/operator-selection-widget/operator-selection-widget.component.ts
@@ -1,22 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit, OnDestroy, Input } from '@angular/core';
 import { ControlWidget } from 'ngx-schema-form';
-import { SupportedFilterTypesService, FilterType } from '../../services/supported-filter-types-service/supported-filter-types.service';
+import { SupportedFilterTypesService, FilterType, FieldType } from '../../services/supported-filter-types-service/supported-filter-types.service';
+import { FieldNode } from '../../services/db-schema-service/db-schema.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'operator-selection-widget',
   templateUrl: './operator-selection-widget.component.html',
   styleUrls: ['./operator-selection-widget.component.css']
 })
-export class OperatorSelectionWidgetComponent extends ControlWidget {
+export class OperatorSelectionWidgetComponent extends ControlWidget implements OnDestroy, AfterContentInit {
+  @Input() fieldType: string;
 
+  private selectedField: FieldNode = null;
   operators: Array<FilterType> = null ;
+
+  private entityFieldSub: Subscription;
 
   constructor(private operatorsService: SupportedFilterTypesService) {
     super();
-    this.operatorsService.getSupportedFilterTypes().subscribe(
-      (data: Array<FilterType>) => this.operators = data // success path
-        // error => this.error = error // error path
-    );
    }
+
+  ngAfterContentInit() {
+
+    const dependentProperty = this.formProperty.searchProperty('field');
+    console.log(dependentProperty);
+
+    this.entityFieldSub = dependentProperty.valueChanges.asObservable().subscribe(
+      (field: FieldNode) => this.fieldChanged(field)
+      // error => this.error = error // error path
+    );
+  }
+
+  ngOnDestroy() {
+    this.entityFieldSub.unsubscribe();
+  }
+
+  getOperatorsOfType(type: FieldType) {
+    const sub: Subscription = this.operatorsService.getFiltersOfType(type).subscribe(
+      (data: Array<FilterType>) => this.operators = data, // success path
+      // error => sub.unsubscribe // error path
+      () => sub.unsubscribe
+    );
+  }
+
+  fieldChanged(field: FieldNode) {
+    if (!field) { return; }
+
+    if (this.selectedField === null || FieldType[field.type] !== FieldType[this.selectedField.type] ) {
+
+      console.log('FieldType Changed : ' + FieldType[field.type]);
+      console.log(field);
+      this.getOperatorsOfType(FieldType[field.type]);
+      this.selectedField = field;
+      // Reset the operator
+      this.control.setValue('');
+
+    } else if ( field.name !== this.selectedField.name) {
+      this.selectedField = field;
+    }
+  }
+
+  operatorChanged(operatorValue: string) {
+
+    console.log('Operator Changed: ' + operatorValue);
+  }
 
 }
