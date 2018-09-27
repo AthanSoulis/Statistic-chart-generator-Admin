bcef4eb5570d4243cbcfc375fd29c53f2295407e
Athan Soulis <athan.soulis@gmail.com>
2018-09-27 18:44:43 +0300

    Update to Entity Selection Widget
    ~ Entity Selection Widget takes into account the profile selected

--- src/app/custom-ngx-schema-form-widgets/entity-selection-widget/entity-selection-widget.component.ts
+++ src/app/custom-ngx-schema-form-widgets/entity-selection-widget/entity-selection-widget.component.ts
@@ -1,24 +1,45 @@
-import { Component, AfterViewInit, Injectable, OnChanges, SimpleChanges, AfterContentInit, OnDestroy, forwardRef, Inject } from '@angular/core';
+import { Component, AfterViewInit, Injectable, OnChanges, SimpleChanges, AfterContentInit, OnDestroy, forwardRef, Inject, OnInit, Output, EventEmitter, ElementRef, ChangeDetectionStrategy } from '@angular/core';
 import { ControlWidget } from 'ngx-schema-form';
 import { DbSchemaService } from '../../services/db-schema-service/db-schema.service';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { MappingProfilesService } from '../../services/mapping-profiles-service/mapping-profiles.service';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'entity-selection-widget',
   templateUrl: './entity-selection-widget.component.html',
-  styleUrls: ['./entity-selection-widget.component.css']
+  styleUrls: ['./entity-selection-widget.component.css'],
 })
-export class EntitySelectionWidgetComponent extends ControlWidget implements AfterContentInit {
+export class EntitySelectionWidgetComponent extends ControlWidget implements OnInit, AfterContentInit, OnDestroy {
+
+  @Output() entityChange: EventEmitter<any> = new EventEmitter();
 
   entities: Array<string>;
+  mappingProfileServiceSubscription: Subscription;
 
-  constructor(private dbSchemaService: DbSchemaService) {
+  constructor(private dbSchemaService: DbSchemaService, private mappingProfileService: MappingProfilesService) {
     super();
+  }
 
-    dbSchemaService.getAvailableEntities(null).subscribe(
-      (data: Array<string>) => this.entities = data // success path
-      // error => this.error = error // error path
-    );
+  ngOnInit() {
+    this.mappingProfileServiceSubscription = this.mappingProfileService.selectedProfile$
+    .subscribe(profile => {
+
+      this.dbSchemaService.getAvailableEntities(profile).pipe(distinctUntilChanged()).subscribe(
+        // success path
+        (data: Array<string>) => {
+          if (!this.mappingProfileService.firstChange) {
+             this.formProperty.reset(null, false);
+          }
+          this.entities = data;
+        }
+        // error => this.error = error // error path
+      );
+    });
+  }
+
+  ngOnDestroy() {
+    this.mappingProfileServiceSubscription.unsubscribe();
   }
 
   ngAfterContentInit() {}
