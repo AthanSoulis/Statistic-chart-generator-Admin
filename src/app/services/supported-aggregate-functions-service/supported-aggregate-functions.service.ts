4e1a7cb1027b382cc517082a29a281e1c0a4b5a4
Athan Soulis <athan.soulis@gmail.com>
2018-12-14 22:19:27 +0200

    Made the aggregate picker values more elegant

--- src/app/services/supported-aggregate-functions-service/supported-aggregate-functions.service.ts
+++ src/app/services/supported-aggregate-functions-service/supported-aggregate-functions.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
+export interface AggregateFunction {
+  name: string;
+  code: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class SupportedAggregateFunctionsService {
 
   constructor() { }
 
-  getSupportedAggregateFunctionFilterY(): Observable<Array<string>> {
-    return of(['total', 'count', 'min', 'max', 'avg']);
+  getSupportedAggregateFunctionFilterY(): Observable<Array<AggregateFunction>> {
+    return of([
+      {name: 'Total', code: 'total'},
+      {name: 'Count', code: 'count'},
+      {name: 'Minimum', code: 'min'},
+      {name: 'Maximum', code: 'max'},
+      {name: 'Average', code: 'avg'}]);
   }
 
-  getSupportedAggregateFunctionFilterX(): Observable<Array<string>> {
+  getSupportedAggregateFunctionFilterX(): Observable<Array<AggregateFunction>> {
     return of([]);
   }
 }
