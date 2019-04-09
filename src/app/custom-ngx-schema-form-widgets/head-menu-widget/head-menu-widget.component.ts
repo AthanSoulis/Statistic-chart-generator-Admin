54d74d71143788f11acc2d5c78c22eb532cc2919
Athan Soulis <athan.soulis@gmail.com>
2019-04-09 19:49:09 +0300

    Head menu step tabs implementation

--- src/app/custom-ngx-schema-form-widgets/head-menu-widget/head-menu-widget.component.ts
+++ src/app/custom-ngx-schema-form-widgets/head-menu-widget/head-menu-widget.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { FormSchema } from '../../chart-creator/chart-form-schema.model';
 import { ObjectLayoutWidget } from 'ngx-schema-form';
+import { MappingProfilesService } from '../../services/mapping-profiles-service/mapping-profiles.service';
+import { DiagramCategoryService } from '../../services/diagram-category-service/diagram-category.service';
+import { TabActivationStatusService } from '../../services/tab-activation-status-service/tab-activation-status.service';
 
 @Component({
   selector: 'head-menu-widget',
@@ -8,7 +10,13 @@ import { ObjectLayoutWidget } from 'ngx-schema-form';
   styleUrls: ['./head-menu-widget.component.css'],
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeadMenuWidgetComponent extends ObjectLayoutWidget {
+export class HeadMenuWidgetComponent extends ObjectLayoutWidget implements OnInit {
 
+  constructor(public mappingProfileService: MappingProfilesService,
+              public diagramCategoryService: DiagramCategoryService,
+              public tabActivationStatusService: TabActivationStatusService) {
+    super();
+  }
 
+  ngOnInit() {}
 }
