c0df02c044e013879c87a2228a7027c23fdec0ce
Athan Soulis <athan.soulis@gmail.com>
2019-04-19 17:33:18 +0300

    Created a new menu with tabs

---
+++ src/app/custom-ngx-schema-form-widgets/tabular-menu-widget/tabular-menu-widget.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { ObjectLayoutWidget } from 'ngx-schema-form';
+
+@Component({
+  selector: 'tabular-menu-widget',
+  templateUrl: './tabular-menu-widget.component.html',
+  styleUrls: ['./tabular-menu-widget.component.css']
+})
+export class TabularMenuWidgetComponent extends ObjectLayoutWidget implements AfterContentInit {
+
+  ngAfterContentInit() {
+  }
+
+  trackByIndex(index: number, item: any) {
+    return item;
+  }
+}
