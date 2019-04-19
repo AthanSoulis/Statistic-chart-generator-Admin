6b7dad7c59e7cae881ff9a44d3e4a0b0cec60747
Athan Soulis <athan.soulis@gmail.com>
2019-04-19 17:58:25 +0300

    Updated the library selection
    It is now based on available libraries of a picked chart

--- src/app/custom-ngx-schema-form-widgets/library-selection-widget/library-selection-widget.component.ts
+++ src/app/custom-ngx-schema-form-widgets/library-selection-widget/library-selection-widget.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { Component, AfterContentInit, ChangeDetectorRef } from '@angular/core';
 import { ControlWidget } from 'ngx-schema-form';
 import { SupportedLibrariesService } from '../../services/supported-libraries-service/supported-libraries.service';
+import { DiagramCategoryService } from '../../services/diagram-category-service/diagram-category.service';
+import { ISupportedCategory } from '../../services/supported-chart-types-service/supported-chart-types.service';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'library-selection-widget',
@@ -10,8 +13,11 @@ import { SupportedLibrariesService } from '../../services/supported-libraries-se
 export class LibrarySelectionWidgetComponent extends ControlWidget implements AfterContentInit {
 
   supportedLibraries: Array<string>;
+  availableLibraries: Array<string>;
 
-  constructor(private librariesService: SupportedLibrariesService) {
+  constructor(private librariesService: SupportedLibrariesService,
+    private diagramCategoryService: DiagramCategoryService,
+    private cdr: ChangeDetectorRef) {
     super();
   }
 
@@ -22,5 +28,19 @@ export class LibrarySelectionWidgetComponent extends ControlWidget implements Af
       (data: Array<string>) => this.supportedLibraries = data
         // error => this.error = error // error path
     );
+
+    this.formProperty.root.searchProperty('/category/categoryType').valueChanges.subscribe(
+      (categoryType: string) => {
+        const availableDiagramsPerCategory =  this.diagramCategoryService.availableDiagrams
+        .find((availableDiagram: ISupportedCategory) =>  availableDiagram.type === categoryType);
+
+        if (isNullOrUndefined(availableDiagramsPerCategory)) {
+          this.availableLibraries = [];
+        } else {
+          this.availableLibraries = availableDiagramsPerCategory.supportedLibraries;
+          this.formProperty.setValue(this.availableLibraries[0], false);
+          this.cdr.markForCheck();
+        }
+      });
   }
 }
