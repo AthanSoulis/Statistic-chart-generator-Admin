023876c72d15662f0bf39ce87571a052be5b0375
root <root@dl040.madgik.di.uoa.gr>
2020-11-27 16:02:28 +0200

    Added build configurations for prod, beta and madgik

---
+++ src/environments/environment.beta.ts
@@ -0,0 +1,5 @@
+export const environment = {
+production: true,
+apiUrl: 'https://beta.services.openaire.eu',
+apiFolder: '/stats-tool'
+};
---
+++ src/environments/environment.madgik.ts
@@ -0,0 +1,5 @@
+export const environment = {
+production: true,
+apiUrl: 'https://stats.madgik.di.uoa.gr',
+apiFolder: '/stats-api'
+};
