7d73ec57f566d8f752a500ad4adfe0347ca03bcb
Athan Soulis <athan.soulis@gmail.com>
2021-01-09 22:04:49 +0200

    Removed unused statements

--- src/app/services/url-provider-service/url-provider.service.ts
+++ src/app/services/url-provider-service/url-provider.service.ts
@@ -4,51 +4,16 @@ import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 import {environment} from '../../../environments/environment';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class UrlProviderService {
 
-  browserWindow = window || {};
-  browserWindowEnv = this.browserWindow['__env'] || {};
-
-  private domain;
-  private port = '8080';
-  private protocol = 'http';
-
-  private url: string;
-  private iframeUrl: string;
-
-  constructor() {
-
-    this.domain = location.hostname;
-    this.url = this.protocol + '://' + this.domain + ':' + this.port;
-    this.iframeUrl = this.protocol + '://' + this.domain + ':' + this.port;
-
-    if (this.browserWindowEnv.hasOwnProperty('apiUrl')) {
-
-      const apiUrl = (<string>window['__env']['apiUrl']);
-      const apiFolder = (<string>window['__env']['apiFolder']);
-
-      if (apiUrl !== '' && apiUrl !== null ) {
-        this.iframeUrl = apiUrl;
-
-        if (apiFolder !== '' && apiFolder !== null) {
-          this.url = apiUrl + apiFolder;
-        } else {
-          this.url = apiUrl;
-        }}
-    }
-  }
-
   get iframeURL(): string {
-  //    return this.iframeUrl;
-  return  environment.apiUrl;
+    return  environment.apiUrl;
   }
 
   get serviceURL(): string {
-  // return this.url;
-  return environment.apiUrl + environment.apiFolder;
+    return environment.apiUrl + environment.apiFolder;
   }
 }
