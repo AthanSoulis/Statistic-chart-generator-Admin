93e0eaf19f62bb6a6c9e9f5efcf0c8f4b9f59ff4
AthanSoulis <athan.soulis@gmail.com>
2021-05-13 20:43:56 +0300

    Fixed opening the reset modal when pressing enter. Fixed not focusing the first dataseries after reseting

--- src/app/custom-ngx-schema-form-widgets/dataseries-menu-widget/dataseries-menu-widget.component.ts
+++ src/app/custom-ngx-schema-form-widgets/dataseries-menu-widget/dataseries-menu-widget.component.ts
@@ -1,8 +1,8 @@
-import { element } from 'protractor';
-import { Component, OnInit, ChangeDetectorRef, OnDestroy, ViewChild, AfterViewInit, ChangeDetectionStrategy, ElementRef } from '@angular/core';
+import { DataseriesFormSchema } from './../../chart-creator/chart-form-schema.classes';
+import { Component, ChangeDetectorRef, OnDestroy, ViewChild, AfterViewInit, ChangeDetectionStrategy, ElementRef } from '@angular/core';
 import { ArrayLayoutWidget } from 'ngx-schema-form';
 import { FormProperty } from 'ngx-schema-form/lib/model/formproperty';
-import { Observable, Subscription, of } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
 import { ChartLoadingService } from '../../services/chart-loading-service/chart-loading.service';
 import { NgbNav, NgbNavItem } from '@ng-bootstrap/ng-bootstrap';
@@ -13,41 +13,47 @@ import { NgbNav, NgbNavItem } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./dataseries-menu-widget.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DataseriesMenuWidgetComponent extends ArrayLayoutWidget implements AfterViewInit, OnDestroy, OnInit {
+export class DataseriesMenuWidgetComponent extends ArrayLayoutWidget implements AfterViewInit, OnDestroy {
 
   @ViewChild('nav', {static: false}) dataseriesMenu:NgbNav;
   
-  @ViewChild('editDataseriesName') set userContent(element: ElementRef) {
-    if (element) {
-      // Here we get access only when element is rendered
+  @ViewChild('editDataseriesName') set userContent(element: ElementRef)
+  {
+    // Here we get access only when element is rendered
+    if (element)
       (element.nativeElement as HTMLElement).getElementsByTagName("input")[0].focus();
-    }
   }
 
   subscriptions: Subscription[] = [];
   public editableName: NgbNavItem | null = null;
 
-  constructor(private cdr: ChangeDetectorRef, private loadingService: ChartLoadingService) 
-  {
-    super();
-  }
-
-  ngOnInit() {
-    this.subscriptions.push(this.formProperty.valueChanges.
-      pipe(distinctUntilChanged()).subscribe(
-      (value: FormProperty[]) => {
-
-        if (this.loadingService.chartLoadingStatus) {
-          // this.dataseriesTabService.active = new Array<boolean>(value.length).fill(false);
-          // this.dataseriesTabService.active[0] = true;
-        }
-      }
-    ));
-  }
+  constructor(private cdr: ChangeDetectorRef, private loadingService: ChartLoadingService) { super(); }
 
   ngAfterViewInit() {
     super.ngAfterViewInit();
+
     this.addItem();
+
+    this.subscriptions.push(this.formProperty.valueChanges.
+      pipe(distinctUntilChanged(this.compareDataseriesSchema)).subscribe(
+      (value: DataseriesFormSchema[]) => {
+        console.log(value);
+        
+        // After a chart load make the last Dataseries active.
+        if (this.loadingService.chartLoadingStatus) {
+          this.dataseriesMenu.activeId = value.length - 1;
+          this.cdr.markForCheck();
+          return;
+        }
+
+        if(value.length > 1)
+          return;
+        
+        // When there is one Dataseries make sure the dataseries menu is active on it.
+        this.dataseriesMenu.activeId = value.length - 1;
+        this.cdr.markForCheck();
+      }
+    ));
   }
 
   ngOnDestroy() {
@@ -70,10 +76,6 @@ export class DataseriesMenuWidgetComponent extends ArrayLayoutWidget implements 
   isEditableName(index: number) : boolean
   {
     return this.editableName !== null && this.editableName.id == index;
-  }
-
-  get menuArrayLength(): number {
-    return (<FormProperty[]>this.formProperty.properties).length;
   }
 
   getDataSeriesName(index: number): Observable<string> | null{
@@ -148,6 +150,33 @@ export class DataseriesMenuWidgetComponent extends ArrayLayoutWidget implements 
 
   trackByIndex(index: number, item: any) {
     return item;
+  }
+
+  public changeOnEnter(event: Event)
+  {
+    event.target.dispatchEvent(new Event("blur"));
+    return true;
+  }
+
+  get menuArrayLength(): number {
+    return (<FormProperty[]>this.formProperty.properties).length;
+  }
+
+  private compareDataseriesSchema(prev: DataseriesFormSchema[], curr: DataseriesFormSchema[])
+  {
+    if(prev.length !== curr.length)
+      return false;
+    for (let i = 0; i < prev.length; i++)
+    {
+      if(prev[i] !== curr[i])
+        return false;
+      
+      if(prev[i].data !== curr[i].data)
+        return false;
+      if(prev[i].chartProperties !== curr[i].chartProperties)
+        return false;
+    }
+    return true;
   }
 
 }
